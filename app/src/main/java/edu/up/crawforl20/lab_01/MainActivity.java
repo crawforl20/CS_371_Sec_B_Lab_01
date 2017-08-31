81d2545760bf1225a2a093eb9e1efa150e7be6b8
crawforl20 <crawforl@up.edu>
2017-08-31 16:47:59 -0700

    Initial commit

---
+++ app/src/main/java/edu/up/crawforl20/lab_01/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.up.crawforl20.lab_01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
